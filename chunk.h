89c928a795a351200af92b8e3c7388b7fcea7b18
landonjw <landonjwdev@gmail.com>
2023-03-20 21:44:35 -0400

    Add constant instruction, line number tracking, and chunk disassembler for debugging

--- chunk.h
+++ chunk.h
@@ -6,8 +6,10 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -15,10 +17,13 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
